7df34d5341776ef625370f3c34e7acfae51843a8
mostafamohamedmohamed <sasacool25@gmail.com>
2026-01-27 22:39:16 +0200

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.brewbloomcoffee.app',
+  appName: 'Brew Bloom Coffee',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
